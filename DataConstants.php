a117d7aff317256efa55008f4ef67c99606f4404
MahmoudFenej <mahmoudfenej@gmail.com>
2017-02-25 16:28:49 +0200

    first test for github

---
+++ DataConstants.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DataConstants
+ *
+ * @author mac
+ */
+abstract class DataConstants {
+  const firstText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+            a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+            including versions of Lorem Ipsum.";
+    
+    
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+        <meta name="Viewport" content="width=device-width,initial-scale=1"/>
+        <title></title>
+    </head>
+    <body>
+
+        <h1>HeadLine</h1>
+        <p><?php echo DataConstants::firstText;?> </p>
+    </body>
+</html>
