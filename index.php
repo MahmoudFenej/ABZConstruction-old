bef832f3090b738dfeb657e8f02fcbb4958adfa5
MahmoudFenej <mahmoudfenej@gmail.com>
2017-02-26 18:54:13 +0200

    Navigation Bar

--- index.php
+++ index.php
@@ -1,17 +1,43 @@
-<?php 
-include 'TextConstants.php';
-?>
-<html>
+<!DOCTYPE html>
+<?php include_once 'Strings/TextConstants.php';?>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
-        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
-        <meta name="Viewport" content="width=device-width,initial-scale=1"/>
-        <title></title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/script.js"></script>
+        <title>ABZ Construction</title>
     </head>
     <body>
 
-      
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#featured">Wisdom <span class="subhead">Pet Medicine</span></a>
+                </div><!-- navbar-header -->
+                <div class="collapse navbar-collapse" id="collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#featured"><?php echo TextConstants::HOME;?></a></li>
+                        <li><a href="#mission"><?php echo TextConstants::OUR_WORK;?></a></li>
+                        <li><a href="#services"><?php echo TextConstants::ABOUT_US;?></a></li>
+                        <li><a href="#staff"><?php echo TextConstants::CONTACT_US;?></a></li>
+                        <li><a href="#testimonials"><?php echo TextConstants::SEND_REQUEST;?></a></li>
+                    </ul>        
+                </div>
+            </div>
+        </nav>
+
+
+
+
     </body>
 </html>
